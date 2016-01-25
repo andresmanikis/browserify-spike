38d672366829411da9de14bed11534b2c47b623a
Andrés Manikis <andresmanikis@gmail.com>
2016-01-25 20:19:41 -0300

    Adds grunt-contrib-clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.initConfig({
-    browserify: require('./grunt-config/browserify.js')
+    browserify: require('./grunt-config/browserify.js'),
+    clean: ['dist']
   });
 
-  grunt.registerTask('build', ['browserify:build']);
-  grunt.registerTask('dev', ['browserify:watch']);
+  grunt.registerTask('build', ['clean', 'browserify:build']);
+  grunt.registerTask('dev', ['clean', 'browserify:watch']);
 };
